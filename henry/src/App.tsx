e3db453566b18d86a9d86251b49cda837963f8f9
H3nryK <henrykimani89@gmail.com>
2024-07-20 20:40:15 +0300

    preloader set

--- henry/src/App.tsx
+++ henry/src/App.tsx
@@ -1,25 +1,42 @@
 // src/App.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Contact from './components/Contact';
 import About from './components/About';
 import Projects from './components/Projects';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
-import { ThemeProvider } from 'next-themes'
+import Preloader from './components/Preloader';
+import { ThemeProvider } from 'next-themes';
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <ThemeProvider>
       <div className="App">
-        <Header />
-        <Hero />
-        <main>
-          <About />
-          <Projects />
-          <Contact />
-        </main>
-        <Footer />
+        {isLoading && <Preloader />}
+        {!isLoading && (
+          <>
+            <Header />
+            <Hero />
+            <main>
+              <About />
+              <Projects />
+              <Contact />
+            </main>
+            <Footer />
+          </>
+        )}
       </div>
     </ThemeProvider>
   );
